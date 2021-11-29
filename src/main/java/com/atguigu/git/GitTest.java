e249b0949fd71988222c0a84333d43b6850d9ad1
Echo18322276280 <95195056+Echo18322276280@users.noreply.github.com>
2021-11-29 11:49:32 +0800

    pullTest

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("mmmmmmmmmmmmmmmmmmmmmm");
         System.out.println("hot-fix");
         System.out.println("hhhhhhhhhhhhhhhhhhhh");
+        System.out.println("pull test");
     }
 }
